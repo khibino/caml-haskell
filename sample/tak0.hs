764570dc78636cf43277e790ade0ef41b9d07006
HIBINO Kei <hibi@utmc.or.jp>
2010-03-08 16:27:44 +0900

    Add simple print sample.

--- sample/tak0.hs
+++ sample/tak0.hs
@@ -26,6 +26,6 @@ taky x y z
                (taky (y - 1) z x)
                (taky (z - 1) x y)
 
-main = print (taky 2 1 1)
+main = print (taky 100 50 0)
 --main = print (taky 0 0 0)
 --main = print (taky 10 3 1)
